5c9f83ce5e928a332c1f437c6dcf3544521bc209
dinream <2534393465@qq.com>
2023-11-16 03:19:03 +0000

    11.16_insert

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -12,16 +12,42 @@
 
 #include <memory>
 
+#include "catalog/catalog.h"
 #include "execution/executors/insert_executor.h"
+#include "storage/table/table_iterator.h"
+#include "storage/table/tuple.h"
 
 namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+    plan_=plan;
+    child_executor_=child_executor.get();
+}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() { 
+    // throw NotImplementedException("InsertExecutor is not implemented"); 
+    
+}
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { 
+    // BufferPoolManager *bpm_=exec_ctx_->GetBufferPoolManager();
+    Catalog* cl = exec_ctx_->GetCatalog();
+    auto &t_heap=cl->GetTable(plan_->TableOid())->table_;
+    auto table= cl->GetTable(plan_->TableOid());
+    // TableIterator t_iter=cl->GetTable(plan_->TableOid())->table_->MakeIterator();  虽然有可以进行插入的tableheap，但是这里的 t_iter 中无法访问到  tableheap，并且远了
+    
+    Tuple tp;
+    RID ri{};
+    // 一个计划节点中会不会有多个值
+    while(child_executor_->Next(&tp, &ri)){ 
+        // 虽然说 t_heap->GetTupleMeta(ri) 这里可以得到 tupleMeta， 但是 ，这里得重新构造一个,
+        
+        t_heap->InsertTuple({0,0,false}, tp);
+    }
+    //t_heap->InsertTuple(const TupleMeta &mta, const Tuple &tule)
+    return true; // child_executor_->Next(&tp, &ri) 神么情况下返回false；
+}
 
 }  // namespace bustub
--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,59 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "buffer/buffer_pool_manager.h"
+#include "catalog/catalog.h"
+#include "common/macros.h"
+#include "storage/index/b_plus_tree.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
+#include "type/value_factory.h"
 
 namespace bustub {
+/**
+* ecec_ctx 执行器上下文，由外部构造，此处保存指针
+* catalog_{catalog},
+  bpm_{bpm}, 比较重要的
+* plan 序列扫描，由外部构造，此处保存指针
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+*/
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+  // throw NotImplementedException("SeqScanExecutor is not implemented");
+  Catalog *cl = exec_ctx_->GetCatalog();
+  t_id_= plan_->GetTableOid();
+  TableInfo *table = cl->GetTable(plan_->GetTableOid());
+  // BUSTUB_ASSERT(table == nullptr, "table is not exist ");
+  t_iter_ = new TableIterator(table->table_->MakeIterator());
+  // t_iter_ = TableIterator((table->table_), RID{INVALID_PAGE_ID, 0}, RID{INVALID_PAGE_ID, 0});
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  while (true) {  // 主要是为了防止 被删除的元组连续出现
+    if (t_iter_->IsEnd()) { 
+      if(t_iter_!= nullptr){ // 防止多次 Next 导致多次释放
+        delete t_iter_;
+        t_iter_ = nullptr;
+      }
+      return false;
+    }
+    // std::cout << "hah" << std::endl;
+    if (t_iter_->GetTuple().first.is_deleted_ || 
+            (plan_->filter_predicate_ != nullptr && 
+            plan_->filter_predicate_->Evaluate(tuple, exec_ctx_->GetCatalog()->GetTable(t_id_)->schema_)
+                .CompareEquals(ValueFactory::GetBooleanValue(false)) == CmpBool::CmpTrue)) { // 出现 where 还需要比较一下
+      ++(*t_iter_);  // 下移
+      continue;
+    }
+    *tuple = t_iter_->GetTuple().second;
+    *rid = t_iter_->GetRID();
+    break;
+  }
+  ++(*t_iter_);
+  // return !((++t_iter_)->IsEnd()); 只是要返回了 数据 ，说明 就是 false
+  return true;
+}
 
 }  // namespace bustub
